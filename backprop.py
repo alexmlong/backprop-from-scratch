c4bf7f369420b7cc7d7392036ff4d4c19a01fc67
Alex Long <alexmasonlong@gmail.com>
2023-08-11 10:51:31 -0400

    Add test for full forward prop

--- backprop.py
+++ backprop.py
@@ -26,3 +26,20 @@ def forward_propagate_one_layer(previous_layer_values, weight_matrix):
             output_value += weight * previous_value
         output_layer_values.append(output_value)
     return output_layer_values
+
+def forward_propagate(input_values, layer_weight_sets):
+    pass
+#     previous_layer_values = input_values
+#     output_layer_values = 
+#     for weight_set in layer_weight_sets:
+#         for input_value in previous_layer_values:
+#             output_cell_value = 0
+#             for weight in weight_set:
+#                 output_cell_value += input_value * weight
+
+            
+#         for input_row in input_values:
+#             for input_col in input_row:
+#             num_weight_cols = weights.shape[1]
+#             for weight_col_index in range(num_weight_cols):
+#                 input_value = 
--- test_backprop.py
+++ test_backprop.py
@@ -14,3 +14,30 @@ def test_forward_propagate_one_layer():
     next_layer_values = forward_propagate_one_layer(previous_layer_values, weight_matrix)
     assert expected_next_layer_values == next_layer_values
 
+def test_forward_propagate():
+    input_values = [2]
+    layer_weight_matrices = [
+        [
+            [1],
+            [0],
+            [2],
+        ],
+        [
+            [1, 1, 0],
+            [0, 1, 0],
+            [2, 1, 1],
+        ],
+        [
+            [3, 2, 1],
+        ]
+    ]
+
+    # 1st hidden layer values:
+    # 2, 0, 4
+    # 2nd hidden layer values:
+    # 2, 0, 8
+
+    expected_output = 6 + 0 + 8
+
+    output = forward_propagate(input_values, layer_weight_matrices)
+    assert expected_output == output
